6d8ad3881a6e7b5254dc5b7cb9ede642978ccd2c
Nikita Shupeyko <webyneter@gmail.com>
2023-12-04 19:20:21 +0100

    Wipe out pre-created Blog artifacts

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}_web/apps/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from {{ cookiecutter.package_name }}_web.apps.main.models import BlogPost
-
-
-@admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin[BlogPost]):
-    """Admin panel example for ``BlogPost`` model."""
--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}_web/apps/main/urls.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}_web/apps/main/urls.py
@@ -1,9 +1,3 @@
-from django.urls import path
-
-from {{ cookiecutter.package_name }}_web.apps.main.views import index
-
 app_name = 'main'
 
-urlpatterns = [
-    path('hello/', index, name='hello'),
-]
+urlpatterns = []
